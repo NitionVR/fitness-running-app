f0e80da4db63afaa27a55561c83fdea2c2277dd3
amaganyane023 <amaganyane023@student.wethinkcode.co.za>
2024-12-04 12:52:25 +0200

    Initialize flutter starter project

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import location
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
