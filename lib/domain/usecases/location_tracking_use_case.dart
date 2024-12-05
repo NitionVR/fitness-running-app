38305cc1b28b5b4c3853ec39b25f24c4d6744849
amaganyane023 <amaganyane023@student.wethinkcode.co.za>
2024-12-05 09:07:50 +0200

    Add the implementation of the location_tracking_usecase

---
+++ lib/domain/usecases/location_tracking_use_case.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import '../entities/route_point.dart';
+
+class LocationTrackingUseCase {
+  final Stream<LocationData> _locationStream;
+
+  LocationTrackingUseCase(this._locationStream);
+
+  Stream<RoutePoint> startTracking() async* {
+    await for (var location in _locationStream) {
+      if (location.latitude != null && location.longitude != null) {
+        yield RoutePoint(
+          LatLng(location.latitude!, location.longitude!), DateTime.now(),
+          accuracy: location.accuracy ?? double.infinity,  // Pass accuracy if available
+        );
+      }
+    }
+  }
+}
