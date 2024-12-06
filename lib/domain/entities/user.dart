be4f30653c0ebaeefb2162923eb941133b45cde7
amaganyane023 <amaganyane023@student.wethinkcode.co.za>
2024-12-06 23:43:22 +0200

    Add user entity to track user details and sessions

---
+++ lib/domain/entities/user.dart
@@ -0,0 +1,42 @@
+// lib/domain/entities/user.dart
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String id;
+  final String email;
+  final String? displayName;
+  final DateTime createdAt;
+  final DateTime lastLogin;
+
+  User({
+    required this.id,
+    required this.email,
+    this.displayName,
+    required this.createdAt,
+    required this.lastLogin,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'displayName': displayName,
+      'createdAt': createdAt.toIso8601String(),
+      'lastLogin': lastLogin.toIso8601String(),
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map['id'] as String,
+      email: map['email'] as String,
+      displayName: map['displayName'] as String?,
+      createdAt: map['createdAt'] is String
+          ? DateTime.parse(map['createdAt'] as String)
+          : (map['createdAt'] as Timestamp).toDate(),
+      lastLogin: map['lastLogin'] is String
+          ? DateTime.parse(map['lastLogin'] as String)
+          : (map['lastLogin'] as Timestamp).toDate(),
+    );
+  }
+}
